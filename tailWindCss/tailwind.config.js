6ca2351ea367bde9ddb3e70829e26c6fc568725f
shohidullah <recursiveeducation@gmail.com>
2025-03-28 21:11:49 +0600

    tailwind-setup

---
+++ tailWindCss/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html", "./*.js"],  // সব HTML ও JS ফাইল স্ক্যান করবে
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
